4e76928b4e2c8881d90dcd6b874dbf1421d197cb
AzmiHwamed <70154469+AzmiHwamed@users.noreply.github.com>
2025-02-26 00:19:17 +0100

    third

--- src/podcast/podcast.module.ts
+++ src/podcast/podcast.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PodcastService } from './podcast.service';
 import { PodcastController } from './podcast.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [PodcastController],
   providers: [PodcastService],
 })
--- src/quizz/quizz.module.ts
+++ src/quizz/quizz.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { QuizzService } from './quizz.service';
 import { QuizzController } from './quizz.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+    imports: [PrismaModule],
+  
   controllers: [QuizzController],
   providers: [QuizzService],
 })
