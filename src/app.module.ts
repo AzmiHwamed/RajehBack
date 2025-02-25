a0d4186285ff0ea0b126e516ebe8046acb367faf
AzmiHwamed <70154469+AzmiHwamed@users.noreply.github.com>
2025-02-26 00:17:20 +0100

    second

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PodcastModule } from './podcast/podcast.module';
 import { StoryModule } from './story/story.module';
 import { QuizzModule } from './quizz/quizz.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [PodcastModule, StoryModule, QuizzModule],
+  imports: [PodcastModule, StoryModule, QuizzModule,PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
